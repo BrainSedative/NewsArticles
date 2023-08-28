4974b930cfe5aa1d016047531769486377b05ac6
BrainSedative <60501673+BrainSedative@users.noreply.github.com>
2023-08-29 01:19:48 +0300

    Изменения в контроллере, добавление новых классов

---
+++ src/NewsArticles/Infrastructure/NewsArticles.Infrastructure.DataAccess/Contexts/Article/Repositories/ArticleRepository.cs
@@ -0,0 +1,25 @@
+﻿using NewsArticles.Application.AppServices.Contexts.Article.Repositories;
+using NewsArticles.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.InfrastructureDataAccess.Contexts.Post.Repositories
+{
+    public class PostRepository : IArticleRepository
+    {
+        public Task<ArticleDto> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return Task.Run(() => new ArticleDto
+            {
+                Id = Guid.NewGuid(),
+                Title = "Статья о чем-то",
+                Description = "Описание",
+                CategoryName = "Полезное",
+                TagNames = new[] { "природа", "праздники" },
+            }, cancellationToken);
+        }
+    }
+}
